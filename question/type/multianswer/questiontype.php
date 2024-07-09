5faaab47c12effa8535f3f87dc1cb9cc76f404ad
Stephan Robotta <stephan.robotta@bfh.ch>
2024-07-09 10:20:15 +0200

    MDL-81952 questions: Fix warning in export of multitype

--- question/type/multianswer/questiontype.php
+++ question/type/multianswer/questiontype.php
@@ -147,7 +147,7 @@ class qtype_multianswer extends question_type {
             // For wrapped questions the maxgrade is always equal to the defaultmark,
             // there is no entry in the question_instances table for them.
             $wrapped->maxmark = $wrapped->defaultmark;
-            $wrapped->category = $question->categoryobject->id;
+            $wrapped->category = $question->categoryobject->id ?? $question->category ?? 0;
             $question->options->questions[$sequence[$wrapped->id]] = $wrapped;
         }
         $question->hints = $DB->get_records('question_hints',
