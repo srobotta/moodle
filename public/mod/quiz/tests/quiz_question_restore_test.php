5f5f44f2ca1046a4333ddb9304bbe15acca5d1c2
Mark Johnson <mark.johnson@catalyst-eu.net>
2025-12-03 09:22:14 +0000

    MDL-86524 quiz: Update restore test with current tag filter format
    The test for restoring a random question from Moodle 3.9 using tags was
    still testing for the pre-4.3 filtercondition format. This needed
    updating to look for the current format.

--- public/mod/quiz/tests/quiz_question_restore_test.php
+++ public/mod/quiz/tests/quiz_question_restore_test.php
@@ -616,11 +616,7 @@ final class quiz_question_restore_test extends \advanced_testcase {
             $setreference = $DB->get_record('question_set_references',
                 ['itemid' => $slot->id, 'component' => 'mod_quiz', 'questionarea' => 'slot']);
             $filterconditions = json_decode($setreference->filtercondition);
-            $tags = [];
-            foreach ($filterconditions->tags as $tagstring) {
-                $tag = explode(',', $tagstring);
-                $tags[] = $tag[1];
-            }
+            $tags = array_map(fn($tag) => $tag->name, \core_tag_tag::get_bulk($filterconditions->filter->qtagids->values));
             $this->assertEquals([], array_diff($randomtags[$slot->slot], $tags));
         }
 
